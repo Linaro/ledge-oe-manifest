23860f2227c32ae4900f3637e160b2b88d6790bd
Maxim Uvarov <maxim.uvarov@linaro.org>
2020-01-15 18:53:57 +0000

    ledge: make it more easy to build multii platform build
    Simplify build to 2 commands:
     MACHINE=ledge-multi-armv7 DISTRO=rpb source ./setup-environment
     bitbake mc:qemuarm:ledge-iot ${FIRMWARE}
    
    Also update environment scripts with hint of the future steps.
    
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- conf/multiconfig/launch_bitbake_multi_arm64.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -
-
-export MULTICONFIG="qemuarm64"
-sed -e "s%^BBMULTICONFIG ?= .*$%BBMULTICONFIG ?= \"${MULTICONFIG}\"%" -i conf/auto.conf
-MACHINE=ledge-qemuarm64 bitbake \
-	mc:qemuarm64:ledge-gateway \
-	mc:qemuarm64:ledge-iot
